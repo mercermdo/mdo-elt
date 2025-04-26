c4e77386e3375dfc67fc509826951e6b274a683b
mercermdo <mercer.west@mdoholdings.com>
2025-04-26 12:10:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,13 +31,14 @@ async function getLastSyncTimestamp() {
   `;
   const [rows] = await bigquery.query({ query });
   if (rows.length > 0 && rows[0].last_sync_timestamp) {
-    const parsedDate = new Date(rows[0].last_sync_timestamp);
-    if (!isNaN(parsedDate)) {
-      return parsedDate.getTime();
+    const timestamp = rows[0].last_sync_timestamp;
+    if (timestamp instanceof Date && !isNaN(timestamp)) {
+      return timestamp.getTime();
     }
   }
   return null;
 }
+
 
 
 
